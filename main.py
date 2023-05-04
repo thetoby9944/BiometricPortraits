4b1498278adaca850848e70b0166c5702cc5a205
thetoby9944 <thetoby@web.de>
2023-05-04 09:47:29 +0200

    Face ehight

--- main.py
+++ main.py
@@ -47,29 +47,28 @@ for face_landmarks in face_landmarks_list:
     x, y, w, h = cv2.boundingRect(np.asarray(face_landmarks["chin"]))
     x_center, y_center = np.asarray(face_landmarks["nose_bridge"]).mean(axis=0)
     aspect_ratio = 35 / 45
-    upper = int((y - h) * 0.8)
-    lower = int((y + h) * 1.1)
+    upper = int(y - h * 1.2)
+    lower = int(y + h * 1.3)
     width = (lower - upper) * aspect_ratio
     left = x_center - width // 2
     right = x_center + width // 2
     crop_img = replaced_background.crop((left, upper, right, lower))
     crop_img = crop_img.resize((413, 531)) # 35 x 45 mm at 300 DPI
-    crop_img.info["dpi"] = 300
 
     st.image(crop_img)
 
     "## Some (non-ai) color enhancement"
     equalized = Image.fromarray(equalize_this(np.array(crop_img)))
+    equalized.info["dpi"] = 300
     st.image(equalized)
-
-
     result_column.image(equalized)
 
     "# Verify"
     equalized = equalized.convert("RGBA")
-
+    "## Head position"
     chin_template = Image.open("Kinnschablone.png").convert("RGBA")
-    top_to_chin = (y + h) - upper
+    height = lower - upper
+    top_to_chin = h * 1.2 + h
     offset = chin_template.height - top_to_chin
     bordered_image = Image.new('RGBA', (img.width, img.height + 200), (255, 0, 0, 0))
     bordered_image.paste(equalized, (0, 200))
